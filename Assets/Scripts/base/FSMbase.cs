7e9305261172887f504882987a43ced0a6f40786
Dripmaster <son342@gmail.com>
2020-01-28 01:48:57 +0900

    코어수호대 첫 시작

---
+++ Assets/Scripts/base/FSMbase.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSMbase : MonoBehaviour
+{
+    public enum State
+    {
+        IDLE = 0,
+        WALK,
+        ATTACK,
+    };
+    public State objectState;
+    public Animator _anim;
+    public bool newState;
+    //
+    public void Awake()
+    {
+        objectState = State.IDLE;
+        _anim = GetComponent<Animator>();
+    }
+    public void OnEnable()
+    {
+        StartCoroutine("FSMmain");
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+
+    }
+
+    public virtual void setState(State s)
+    {
+        newState = true;
+        objectState = s;
+        _anim.SetInteger("State", (int)objectState);
+    }
+
+
+    IEnumerator FSMmain()
+    {
+
+        while (true)
+        {
+            newState = false;
+            yield return StartCoroutine(objectState.ToString());
+        }
+    }
+    IEnumerator IDLE()
+    {
+        
+
+        do
+        {
+            yield return null;
+
+        } while (!newState);
+    }
+
+    IEnumerator WALK()
+    {
+        
+
+        do
+        {
+            yield return null;
+
+        } while (!newState);
+    }
+
+}
---
+++ Assets/Scripts/base/mobFSM.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mobFSM : FSMbase
+{
+
+
+    public enum State
+    {
+        IDLE = 0,
+        WALK,
+    };
+    public enum Direction
+    {
+        LEFT = 0,
+        RIGHT,
+        UP,
+        DOWN
+    };
+    State mobState;
+    Direction mobDir;
+    Vector2 moveDir;
+    public Transform[] targetVector;
+    int currentTarget=1;
+    public float mobSpeed;
+
+    public void Awake()
+    {
+        base.Awake();
+        mobDir = Direction.DOWN;
+        setState(State.WALK);
+        setDir(mobDir);
+    }
+    public void Update()
+    {
+
+    }
+    public void setState(State s)
+    {
+        newState = true;
+        mobState = s;
+        _anim.SetInteger("State", (int)mobState);
+        _anim.SetInteger("Dir", (int)mobDir);
+
+    }
+    public void setDir(Direction d)
+    {
+        mobDir = d;
+        _anim.SetInteger("Dir", (int)mobDir);
+        _anim.speed = mobSpeed;
+    }
+    IEnumerator FSMmain()
+    {
+
+        while (true)
+        {
+            newState = false;
+            yield return StartCoroutine(mobState.ToString());
+        }
+    }
+    IEnumerator IDLE()
+    {
+
+       
+        do
+        {
+            yield return null;
+            
+
+        } while (!newState);
+    }
+
+    IEnumerator WALK()
+    {
+
+        do
+        {
+            yield return null;
+            Vector2 targetPos = targetVector[currentTarget].position;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime*mobSpeed);
+            
+            if (Vector2.Distance(transform.position, targetPos) < 0.01f)
+            {
+                currentTarget++;
+                if (currentTarget >= targetVector.Length) {
+                    currentTarget = 0;
+                }
+
+                switch (currentTarget) {
+                    case 0: setDir(Direction.LEFT); break;
+                    case 1: setDir(Direction.DOWN); break;
+                    case 2: setDir(Direction.RIGHT); break;
+                    case 3: setDir(Direction.UP); break;
+                    default:setDir(Direction.LEFT); break;
+                }
+                
+            }
+            
+            
+        } while (!newState);
+    }
+    IEnumerator ATTACK()
+    {
+        do
+        {
+            yield return null;
+
+        } while (!newState);
+    }
+}
